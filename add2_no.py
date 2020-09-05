d4700a04ad4390f2f8dc0224c0548c7fe72bb220
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-05 11:56:00 +0530

    added color to error message

--- add2_no.py
+++ add2_no.py
@@ -1,10 +1,12 @@
+from termcolor import colored
+
 while True:
 	try:
 		num1 = float(input('Enter 1st Number: '))
 		num2 = float(input('Enter 2nd Number: '))
 		break
 	except ValueError:
-		print('\nEnter a valid number\n')
+		print(colored('\nEnter a valid number\n', 'red'))
 	except KeyboardInterrupt:
 		print('\n\nExiting')
 		exit()
