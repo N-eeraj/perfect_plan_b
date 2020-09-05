f2ce298484bf9048b188abfedf3eb3f957d926da
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-05 11:51:15 +0530

    error msg

--- add2_no.py
+++ add2_no.py
@@ -1,5 +1,9 @@
-num1 = float(input('Enter 1st Number: '))
-num2 = float(input('Enter 2nd Number: '))
+try:
+	num1 = float(input('Enter 1st Number: '))
+	num2 = float(input('Enter 2nd Number: '))
+except:
+	print('\nEnter a valid number\n')
+	exit()
 
 result = num1 + num2
 
