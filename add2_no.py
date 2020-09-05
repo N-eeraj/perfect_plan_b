c4bf2a179a751f76091943061e396638b5fc1cf0
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-05 11:53:08 +0530

    added loop & modified except condition

--- add2_no.py
+++ add2_no.py
@@ -1,9 +1,10 @@
-try:
-	num1 = float(input('Enter 1st Number: '))
-	num2 = float(input('Enter 2nd Number: '))
-except:
-	print('\nEnter a valid number\n')
-	exit()
+while True:
+	try:
+		num1 = float(input('Enter 1st Number: '))
+		num2 = float(input('Enter 2nd Number: '))
+		break
+	except ValueError:
+		print('\nEnter a valid number\n')
 
 result = num1 + num2
 
