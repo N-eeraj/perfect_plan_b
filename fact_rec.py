461c363452c0816ef4bd2bbf6e3dbdbae9ebcab6
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-07 11:12:30 +0530

    added comments

--- fact_rec.py
+++ fact_rec.py
@@ -1,20 +1,23 @@
-from termcolor import colored
+from termcolor import colored #module for colored output
 
-def fact(x):
-	x_fact = 1
+def fact(x): #recursive function to calculate factorial
+	x_fact = 1 #initializing factorial variable
 	if x != 1:
-		x_fact = x * fact(x - 1)
-		return x_fact
-	return 1
+		x_fact = x * fact(x - 1) #calling the recursive function
+		return x_fact #return value of the factorial
+	return 1 #returning factorial value of 1
 
-while True:
-	try:
-		num = int(input('Enter the number: '))
-		break
-	except ValueError:
+while True: #infinite loop till a valid input is received
+	try: #trying to read valid input
+		num = int(input('Enter the number: ')) #valid input read
+		break #exiting the loop
+	except ValueError: #handling non integer input
 		print(colored('Enter a valid integer', 'red'))
-	except KeyboardInterrupt:
+	except KeyboardInterrupt: #handling keyboard inerrupt to exit
 		print(colored('\nExiting', 'white'))
 		exit()
 
-print(fact(num))
+print(str(num) + '! = ',fact(num)) #printing output along input
+
+#output format :
+#	input! = factorial
