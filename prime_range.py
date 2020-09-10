61a1eb13bae934b48f1754b61e4857d26d664dcc
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-10 14:55:40 +0530

    prime numbers in a range

--- prime_range.py
+++ prime_range.py
@@ -1,5 +1,14 @@
 import read
 
-num1 = read.Read(int)
+def is_prime(num):
+	for i in range(2, num):
+		if num % i == 0:
+			return False
+	return True
 
-print(num1)
+num1 = read.Read(int, 'Lower Limit')
+num2 = read.Read(int, 'Upper Limit')
+
+for num in range(num1, num2 + 1):
+	if is_prime(num):
+		print(num)
