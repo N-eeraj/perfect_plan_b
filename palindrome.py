10e9e845234ff8bff7c9ede78997adb05507cb89
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-13 21:52:29 +0530

    added comments

--- palindrome.py
+++ palindrome.py
@@ -2,9 +2,9 @@ import read
 
 string = read.Read(str, 'String')
 
-rev = string[::-1]
+rev = string[::-1] #reversing string
 
-if string.lower() == rev.lower():
+if string.lower() == rev.lower(): #checking if input string is same as it's reverse
 	print(string, 'is Palindrome')
 else:
 	print(string, 'is not Palindrome')
--- swap_list.py
+++ swap_list.py
@@ -3,5 +3,5 @@ import read
 lst = read.ReadList()
 
 print(lst)
-lst[0],lst[-1] = lst[-1],lst[0]
+lst[0],lst[-1] = lst[-1],lst[0] #swaping 1st & last elements
 print(lst)
