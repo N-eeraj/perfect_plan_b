c3f552e3671425c1905d3339bebe1b4d6fbf5646
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-18 14:24:32 +0530

    Completed Tower of Hanoi

--- hanoi.py
+++ hanoi.py
@@ -4,12 +4,12 @@ import os
 def tower(twr):
 	for i in twr:
 		print(i)
-	print()
-
 def show():
 	os.system('clear')
-	for i in twrs:
-		tower(i)
+	for i,j in enumerate(twrs):
+		tower(j)
+		print('Tower', i + 1)
+		print()
 
 global limit
 limit  = read.Read(int, 'Limit')
@@ -25,14 +25,23 @@ show()
 
 while win not in twrs[1:]:
 	twr_s = read.Read(int, 'Tower number to select') - 1
-	twr_d = read.Read(int, 'Destination tower number') - 1
-	if (twr_s not in [0, 1, 2] and twr_d not in [0, 1, 2]):
+	if not (-1 < twr_s < 3):
 		read.error('Enter number in (1,2,3)')
 		continue
-	if twrs[twr_d] == []:
-		pass
-	elif twrs[twr_d][0] < twrs[twr_s][0]:
-		read.error("Can't place larger number on smaller number")
+	twr_d = read.Read(int, 'Destination tower number') - 1
+	if not (-1 < twr_s < 3):
+		read.error('Enter number in (1,2,3)')
 		continue
-	twrs[twr_d].insert(0, twrs[twr_s].pop(0))
-	show()
+	try:
+		if twrs[twr_d] == []:
+			pass
+		elif twrs[twr_d][0] < twrs[twr_s][0]:
+			read.error("Can't place larger number on smaller number")
+			continue
+		twrs[twr_d].insert(0, twrs[twr_s].pop(0))
+		show()
+	except IndexError:
+		read.error("Can't select from empty tower")
+		continue
+
+print('You Won!!')
